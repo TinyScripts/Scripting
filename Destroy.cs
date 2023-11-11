04f108185c477f7046993c9d74f8507d3d81b515
TinyScripts <107075217+TinyScripts@users.noreply.github.com>
2023-11-11 14:04:59 +0300

    Destroy object in scene script
    Destroy object

---
+++ Destroy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Destroy the entire object
+        Destroy(gameObject);
+    }
+}
