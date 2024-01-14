a05cec3b21a363f418b36104e02aefe31cb49f18
TinyScripts <107075217+TinyScripts@users.noreply.github.com>
2024-01-14 12:01:45 +0300

    Update Trampoline.cs

--- Trampoline.cs
+++ Trampoline.cs
@@ -7,12 +7,10 @@ public class Trampoline : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         other.GetComponent<Jump>().jumpStrength = 10;
-
     }
 
     void OnTriggerExit(Collider other)
     {
         other.GetComponent<Jump>().jumpStrength = 2;
-
     }
 }
